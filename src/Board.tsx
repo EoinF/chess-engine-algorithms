5a0788a3c97003fe843fae006f4a8af5b3ca063d
Eoin Flanagan <flanagep@tcd.ie>
2026-08-22 17:21:31 +0100

    Render chess pieces on board

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,14 +1,58 @@
 import './Board.css';
 
-type BoardCellProps = {
-    piece: string;
+import BlackKing from "./assets/pieces/Chess_kdt45.svg?react";
+import BlackQueen from "./assets/pieces/Chess_qdt45.svg?react";
+import BlackRook from "./assets/pieces/Chess_rdt45.svg?react";
+import BlackBishop from "./assets/pieces/Chess_bdt45.svg?react";
+import BlackKnight from "./assets/pieces/Chess_ndt45.svg?react";
+import BlackPawn from "./assets/pieces/Chess_pdt45.svg?react";
+import WhiteKing from "./assets/pieces/Chess_klt45.svg?react";
+import WhiteQueen from "./assets/pieces/Chess_qlt45.svg?react";
+import WhiteRook from "./assets/pieces/Chess_rlt45.svg?react";
+import WhiteBishop from "./assets/pieces/Chess_blt45.svg?react";
+import WhiteKnight from "./assets/pieces/Chess_nlt45.svg?react";
+import WhitePawn from "./assets/pieces/Chess_plt45.svg?react";
+import { blackBishop, blackKing, blackKnight, blackPawn, blackQueen, blackRook, emptyCell, whiteBishop, whiteKing, whiteKnight, whitePawn, whiteQueen, whiteRook, type BoardCellState } from './state';
+
+
+const GetCellStateComponent = (cellState: BoardCellState) => {
+    switch(cellState) {
+        case emptyCell:
+            return () => null;
+        case blackKing:
+            return BlackKing;
+        case blackQueen:
+            return BlackQueen;
+        case blackRook:
+            return BlackRook;
+        case blackBishop:
+            return BlackBishop;
+        case blackKnight:
+            return BlackKnight;
+        case blackPawn:
+            return BlackPawn;
+        case whiteKing:
+            return WhiteKing;
+        case whiteQueen:
+            return WhiteQueen;
+        case whiteRook:
+            return WhiteRook;
+        case whiteBishop:
+            return WhiteBishop;
+        case whiteKnight:
+            return WhiteKnight;
+        case whitePawn:
+            return WhitePawn;
+    }
 }
 
-const BoardCell = ({piece}: BoardCellProps) => {
-    if (piece === "O") {
-        return <div className="board-cell"/>
-    }
-    return <div className="board-cell">{piece}</div>
+type BoardCellProps = {
+    cellState: BoardCellState;
+}
+
+const BoardCell = ({cellState}: BoardCellProps) => {
+    const CellStateComponent = GetCellStateComponent(cellState)
+    return <div className="board-cell"><CellStateComponent /></div>
 }
 
 const boardRows = Array(8).fill(0).map((_, i) => i);
@@ -24,9 +68,9 @@ export const Board = ({boardState}: BoardProps) => {
 
     return <div className="board">
         {boardRows.map(rowIndex => (
-            <div className="board-row">
+            <div key={rowIndex} className="board-row">
                 {boardRows.map(columnIndex => (
-                    <BoardCell key={rowIndex * 8 + columnIndex} piece={boardState[rowIndex * 8 + columnIndex]} />
+                    <BoardCell key={columnIndex} cellState={boardState[rowIndex * 8 + columnIndex] as BoardCellState} />
                 ))
                 }
             </div>)
--- src/state.ts
+++ src/state.ts
@@ -1,6 +1,32 @@
-const pawn = "P";
-const knight = "N"
-const bishop = "B"
-const rook = "R";
-const queen = "Q"
-const king = "K";
+export const blackPawn = "P";
+export const blackKnight = "N";
+export const blackBishop = "B";
+export const blackRook = "R";
+export const blackQueen = "Q";
+export const blackKing = "K";
+
+export const whitePawn = "p";
+export const whiteKnight = "n";
+export const whiteBishop = "b";
+export const whiteRook = "r";
+export const whiteQueen = "q";
+export const whiteKing = "k";
+
+
+export type GamePiece = typeof blackPawn |
+typeof blackKnight |
+typeof blackBishop |
+typeof blackRook |
+typeof blackQueen |
+typeof blackKing |
+typeof whitePawn |
+typeof whiteKnight |
+typeof whiteBishop |
+typeof whiteRook |
+typeof whiteQueen |
+typeof whiteKing;
+
+export const emptyCell = "O";
+type EmptyCell = typeof emptyCell
+
+export type BoardCellState = GamePiece | EmptyCell;
